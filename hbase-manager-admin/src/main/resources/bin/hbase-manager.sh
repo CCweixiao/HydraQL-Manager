597bf7c7258788cee2cac3cbe96fde2f2e683891
weixiaotome <leojie1314@gmail.com>
2020-11-02 21:42:41 +0800

    :beers: 主页页面更新.

--- hbase-manager-admin/src/main/resources/bin/hbase-manager.sh
+++ hbase-manager-admin/src/main/resources/bin/hbase-manager.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-AppName=hbase-manager-1.0.0.jar
+AppName=hbase-manager-2.0.3.jar
 
 #JVM参数
 JVM_OPTS="-Dname=$AppName  -Duser.timezone=Asia/Shanghai -Xms512M -Xmx512M -XX:PermSize=256M -XX:MaxPermSize=512M -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
